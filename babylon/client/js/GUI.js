6c28638054f3471df99a1b8da31a800ed82e98b2
Omer Nadeem <omernadeem3@gmail.com>
2017-03-28 11:26:51 -0400

    style(GUI spacing adjustment)

--- babylon/client/js/GUI.js
+++ babylon/client/js/GUI.js
@@ -20,7 +20,7 @@ export default function createGUI(composerData) {
   let options = { w: window.innerWidth * .5, h: window.innerHeight * .75, x: guisystem.getCanvasSize().width * 0.3, y: guisystem.getCanvasSize().height * 0.2, heightTitle:40, textTitle: composerName, titleFontSize: 22, colorContent: 'rgb(24, 24, 24)', backgroundColor: 'black', closeButton: null };
   let dialog = new CASTORGUI.GUIWindow("dialog", options, guisystem);
   dialog.setVisible(true);
-  let text = new CASTORGUI.GUIText("textDialog", { size: 20, color:'white', police: 'Palatino Linotype',text: composerTime + composerBirthday + composerBirthCountry + composerDescription, centerHorizontal:true }, guisystem, false);
+  let text = new CASTORGUI.GUIText("textDialog", { size: 20, color:'white', police: 'Palatino Linotype',text: composerTime + composerBirthday + composerBirthCountry + composerDescription, centerHorizontal: true }, guisystem, false);
   dialog.add(text);
 
   //add spotify, takes in name & id to append player to

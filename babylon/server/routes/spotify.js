985d76dd7843932e8b70787ec4400d0b00c4cf19
dahlki <dahlki78@gmail.com>
2017-07-12 21:41:47 -0400

    fix(spotify): fixed broken api & added middleware

---
+++ babylon/server/routes/spotify.js
@@ -0,0 +1,43 @@
+const router = module.exports = require('express').Router()
+const { client_id, client_secret } = require('../../../config.js');
+const Promise = require('bluebird')
+const request = Promise.promisify(require('request'));
+
+Promise.promisifyAll(request);
+
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+router.use('/:name', (req, res, next) => {
+  const artist = req.params.name;
+
+  request.post(authOptions, (error, response, body) => {
+
+    if (!error && response.statusCode === 200) {
+      const token = body.access_token; // use the access token to access the spotify web API
+      const options = {
+        url: `https://api.spotify.com/v1/search?q=${artist}&type=artist`,
+        headers: {
+          'Authorization': `Bearer ${token}`
+        },
+        json: true
+      };
+
+      request.getAsync(options)
+      .then((response) => {
+        const artistData = response.body.artists.items[0];
+        res.send(artistData);
+      })
+      .catch(next)
+    }
+
+  })
+});
